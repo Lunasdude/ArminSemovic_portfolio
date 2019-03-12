9535806fd89b50acc896b70dc85209e6f347978e
Lukas Lennstring <lulapwnie@gmail.com>
2019-03-12 12:28:10 +0100

    Adds js for gallery scroll

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  var target = null;
+
+  $(window).scroll(function(){
+    var scrollFromTop = parseInt($(window).scrollTop());
+    if (scrollFromTop < 300) { target = "#gallery1"; }else{ target = "#gallery2"; }
+  });
+
+  $('#right-button').click(function() {
+    event.preventDefault();
+    $(target).animate({
+      scrollLeft: "+=350px"
+    }, "slow");
+  });
+
+  $('#left-button').click(function() {
+    event.preventDefault();
+    $(target).animate({
+      scrollLeft: "-=350px"
+    }, "slow");
+  });
+});
